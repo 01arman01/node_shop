751e48726e618bf88efd25aec2bb8dec5aa06335
ArmanPapyan <49716257+ArmanPapyan@users.noreply.github.com>
2019-12-09 00:50:26 +0400

    add handlebars and materialize

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expHbs = require('express-handlebars')
+const Sequelize = require('sequelize')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+const app = new express()
+const hbs = expHbs.create({
+    defaultLayout:"main",
+    extname:"hbs"
+
+})
+app.engine('hbs',hbs.engine)
+app.set('view engine','hbs')
+app.set('views','views')
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render("index")
+})
+
+app.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`)
+})
+
+
